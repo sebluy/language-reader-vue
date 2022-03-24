6f97475ae8bda11e95e5f385e2528deff4df985a
Sebastian Luy <sluy@competitive-energy.com>
2022-03-24 12:23:20 +0100

    Add basic definition input

---
+++ src/components/DefinitionInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["tag"]);
+let cTag = computed(() => (props.tag ? props.tag : "input"));
+</script>
+
+<template>
+  <div id="props.id">
+    <span>...</span>
+    <component :is="cTag" type="text" placeholder="Definition" />
+    <button>Google Translate</button>
+  </div>
+</template>
--- src/components/TextReader.vue
+++ src/components/TextReader.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MainWindow from "@/components/MainWindow.vue";
 import TextView from "@/components/TextView.vue";
+import DefinitionInput from "@/components/DefinitionInput.vue";
 
 const props = defineProps(["sentences", "page"]);
 const emit = defineEmits(["changePageBy"]);
@@ -13,7 +14,10 @@ const emit = defineEmits(["changePageBy"]);
     </template>
     <template v-slot:sidebar>
       <div class="sidebar right">
-        <div class="sidebar-group"></div>
+        <div class="sidebar-group">
+          <DefinitionInput id="word-definition" />
+          <DefinitionInput id="sentence-definition" tag="textarea" />
+        </div>
         <div class="sidebar-group">
           <button @click="emit('changePageBy', -1)">Previous Page</button>
           <button @click="emit('changePageBy', 1)">Next Page</button>
