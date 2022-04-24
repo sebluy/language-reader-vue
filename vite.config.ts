7fcd1d28997f0bec08c4bc3cafc296cc16e351d4
Sebastian Luy <sluy@competitive-energy.com>
2022-04-24 18:56:48 +0200

    Change index to work with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  base: "/language-reader-vue/",
+  build: {
+    outDir: ".",
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
